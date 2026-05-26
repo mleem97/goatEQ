285c6e7f828e1519e54b942f57887df671f15415
mleem97 <marvinleede@gmail.com>
2026-05-26 23:39:44 +0200

    fix: Firefox-compatible background.js, fixed manifest (data_collection_permissions, remove offscreen/tabCapture)

--- firefox/build.sh
+++ firefox/build.sh
@@ -4,8 +4,9 @@ cd "$(dirname "$0")/.."
 VERSION=$(jq -r '.version' firefox/manifest.json)
 mkdir -p dist
 tmpdir=$(mktemp -d)
-cp firefox/manifest.json sw.js bg.js popup.html popup.js popup.css \
-   offscreen.html snap.svg-min.js \
+cp firefox/manifest.json firefox/background.js \
+   popup.html popup.js popup.css \
+   snap.svg-min.js \
    goateq16.png goateq32.png goateq48.png goateq64.png goateq128.png "$tmpdir/"
 cd "$tmpdir" && zip -r "$OLDPWD/dist/goatEQ-v${VERSION}-firefox.xpi" . && cd "$OLDPWD"
 rm -rf "$tmpdir"
